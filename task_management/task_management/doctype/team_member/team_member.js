2e74086713b7c42f8276ec46c5bc8dd936ded959
Ashish Raj Purohit <73710123+mrmechi@users.noreply.github.com>
2022-09-28 14:29:16 +0530

    Update team_member.js

--- task_management/task_management/doctype/team_member/team_member.js
+++ task_management/task_management/doctype/team_member/team_member.js
@@ -1,30 +1,30 @@
-// // // // // Copyright (c) 2022, ASHISH and contributors
-// // // // // For license information, please see license.txt
+// Copyright (c) 2022, ASHISH and contributors
+// For license information, please see license.txt
 
 
 
-// let d = new frappe.ui.Dialog({
-//     title: 'Enter details',
-//     fields: [
-//         {
-//             label: 'Member Name',
-//             fieldname: 'member_name',
-//             fieldtype: 'Data'
-//         },
-//         {
-//             label: 'Team Lead',
-//             fieldname: 'team_lead',
-//             fieldtype: 'Data'
-//         },
+let d = new frappe.ui.Dialog({
+    title: 'Enter details',
+    fields: [
+        {
+            label: 'Member Name',
+            fieldname: 'member_name',
+            fieldtype: 'Data'
+        },
+        {
+            label: 'Team Lead',
+            fieldname: 'team_lead',
+            fieldtype: 'Data'
+        },
     
-//     ],
-//     primary_action_label: 'On Save',
-//     primary_action(a) {
-//         cur_frm.doc.member_name = a.member_name;
-//         cur_frm.doc.team_lead = a.team_lead;
-//         cur_frm.refresh();
-//         d.hide();
-//     }
-// });
+    ],
+    primary_action_label: 'On Save',
+    primary_action(a) {
+        cur_frm.doc.member_name = a.member_name;
+        cur_frm.doc.team_lead = a.team_lead;
+        cur_frm.refresh();
+        d.hide();
+    }
+});
 
-// d.show();
+d.show();
